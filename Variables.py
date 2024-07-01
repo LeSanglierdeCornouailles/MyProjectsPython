53c962b1547ddf982eef39571a7d8b7a9de48f79
Victor Gonzalez <victor.gonzalez.cesped@gmail.com>
2024-07-01 07:55:57 +0100

    spell

---
+++ Variables.py
@@ -0,0 +1,20 @@
+# Creating and assigning value to a variable. Data type 'integer'.
+price = 15
+print(price, 'is an Integer')
+
+# Creating and assigning value to a variable. Data type 'floating'.
+raising = 15.5
+print(raising,'is a floating')
+
+# Creating and assigning value to a variable. Data type 'string'.
+name = 'Victor'
+print(name,'is a string')
+
+# Creating and assigning value to a variable. Data type 'string'.
+# Keep in mind that concatenation is only used for strings,
+name1 = 'Victor1'
+print(name1 + 'is a string')
+
+# Creating and assigning value to a variable. Data type 'Boolean'.
+is_published = False
+print(is_published,'is a boolean')
---
+++ working_with_input.py
@@ -0,0 +1,9 @@
+name = input('What is your name ? ')
+fav_color = input('What is your favorite color ? ')
+print('Hi ' + name + '. It looks like you love ' + fav_color )
+print('Thats cool. Lets move to next step')
+
+birth_year = int(input('What year were you born ? '))
+birth_city = input('Where were you born ? ')
+age = (2024 - birth_year)
+print('Hi, ' + name + ' it looks like you are ', age , ' and you were born in ' + birth_city )
