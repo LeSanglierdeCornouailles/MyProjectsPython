2fb84011ebd47b4fcce4f6d02e819baf870d8b80
Victor Gonzalez <victor.gonzalez.cesped@gmail.com>
2024-07-03 19:21:32 +0100

    4

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+#fruits = ["Apple", "Appricot", "Grapes", "Banana"]
+#for fruit in fruits:
+#    print(fruit)
+
+#vegetable = input("Please enter your vegetable:")
+#for x in vegetable:
+#    print(x)
+
+city = input("Please enter a city : ")
+for x in city:
+    if x == "i":
+        break
+    print("Your city contain an i ")
+
