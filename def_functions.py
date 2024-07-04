75d7e0a314fd4dcf29d319c50bd8ad2b7414c30f
Victor Gonzalez <victor.gonzalez.cesped@gmail.com>
2024-07-04 18:18:37 +0100

    6

---
+++ def_functions.py
@@ -0,0 +1,16 @@
+#def Printing_Name (name):
+#    print("Hello", name)
+
+#asking_name = Printing_Name(input("What is you name ?"))
+
+#def calculate_age (year_of_birth):
+#    age = 2024 - int(year_of_birth)
+#    print(f"You are  {age}" + " years old")
+
+#year_of_birth = calculate_age(input("Which year were you born ? "))
+
+def coverting_weight_unit (weight_input):
+    weight_in_lb = 4.5 * int(weight_input)
+    print(f"Your weight {weight_in_lb}" + " Kg")
+
+weight_input = coverting_weight_unit(input("What is your weight in lb ?"))
