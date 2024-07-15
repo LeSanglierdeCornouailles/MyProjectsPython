d683aa6469590b7e2f0985f9c92230db39cafd7e
Victor Gonzalez <victor.gonzalez.cesped@gmail.com>
2024-07-15 12:10:03 +0100

    15_07

--- 3th_exe_if_steta.py
+++ 3th_exe_if_steta.py
@@ -7,3 +7,17 @@ elif N_cha_name > 5:
     print("5 characters max")
 else:
     print("name looks good")
+
+'''
+input_currency = input ("Enter currency to convert :")
+splited_input = input_currency.lower().split()
+
+if splited_input[-1] == "euros": 
+    eu_to_dollar = 1.09 * int(splited_input[0])
+    print(f"{eu_to_dollar}" + " Euros")
+elif splited_input[-1] == "dollars":
+    dollar_to_eu = 0.92 * int(splited_input[0])
+    print(f"{dollar_to_eu}" + " Dollars")
+else:
+    print("You need to enter a number and a symbol in plurial")
+'''
--- def_functions.py
+++ def_functions.py
@@ -9,8 +9,16 @@
 
 #year_of_birth = calculate_age(input("Which year were you born ? "))
 
-def coverting_weight_unit (weight_input):
-    weight_in_lb = 4.5 * int(weight_input)
-    print(f"Your weight {weight_in_lb}" + " Kg")
+#def coverting_weight_unit (weight_input):
+#    weight_in_lb = 4.5 * int(weight_input)
+#    print(f"Your weight {weight_in_lb}" + " Kg")
 
-weight_input = coverting_weight_unit(input("What is your weight in lb ?"))
+#weight_input = coverting_weight_unit(input("What is your weight in lb ?"))
+
+
+
+
+
+
+
+
