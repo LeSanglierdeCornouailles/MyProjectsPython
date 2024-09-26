55efbba010b4f1a5aa8180af4922d09622651377
Victor Gonzalez <victor.gonzalez.cesped@gmail.com>
2024-09-26 12:03:24 +0100

    a

--- playing_with_WhileLoop2.py
+++ playing_with_WhileLoop2.py
@@ -1,19 +1,19 @@
 
 user_command = ""
 
-while True: 
-    user_command = input("Enter a command !").lower()
-    if user_command == "Start":
+while user_command != "quit": 
+    user_command = input("Enter a command ! : ").lower()
+    if user_command == "start":
         print("Car Starting")
-    elif user_command =="Stop":
+    elif user_command == "stop":
         print("Car Stopped")
-    elif user_command =="Help":
+    elif user_command == "help":
         print("""
 Start - for starting a car
 Stop  - for stopping the car
 Quit  - for exiting the program
         """)
-    elif user_command == "Quit":    
+    elif user_command == "quit":    
         break
     else:
         print("Sorry add one of the command")
